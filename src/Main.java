2d2b5231f26db76cf7ec42e9e36bc8e6637b19e5
«Margarita <korolmargo70@gmail.com>
2023-04-19 00:09:09 +0300

    3DZ2.1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketAmount = 60;
+        int oneMili = 20;
+
+        int Mili = ticketAmount/oneMili;
+
+
+        System.out.println("Количество миль " + Mili);
+    }
+}
